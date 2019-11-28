376d9896bdc634dee1da0666ab8cf4c33f209b58
Julio Barra <48953146+Fitinpool@users.noreply.github.com>
2019-11-27 23:11:46 -0300

    Add files via upload

--- Laboratorio3.c
+++ Laboratorio3.c
@@ -25,7 +25,7 @@ int main(){
     char eleccionJugador = 2;
     char tablero[tam][tam];
     char tableroEnJuego[tam][tam];
-    char cuentaMinas = '0';
+    char cuentaMinas;
 
     //inicio
     Titulo();
@@ -65,8 +65,11 @@ int main(){
     LimpiaPantalla();
 
     while(ganarJuego != minas){
-        //mostrarMinas = 1;
-        //mostrar_tablero(tableroEnJuego, mostrarMinas);
+
+        /*
+        mostrarMinas = 1;
+        mostrar_tablero(tableroEnJuego, mostrarMinas);
+        */
         mostrar_tablero(tableroEnJuego, mostrarMinas);
 
         printf("Ingrese M para marcar, A para abrir una casilla: ");
@@ -85,7 +88,7 @@ int main(){
 
         //printf("\n %i,%i \n", fila, columna);
 
-        //revisa si hay una mina, si no la hay coloca la cantidad de minas que hay alrededor
+        //revisa si hay una minas alrededor y guarda la cantidad de minas en el arreglo
         if(eleccionJugador == 'a' || eleccionJugador == 'A'){
             if(tablero[fila][columna] == '*'){
                 mostrarMinas = 1;
@@ -94,6 +97,7 @@ int main(){
 
             }
             else{
+                cuentaMinas = '0';
                 for(i = fila - 1; i <= fila + 1; i++){
                     for(j = columna - 1; j <= columna + 1; j++){
                         if(tablero[i][j] == '*'){
@@ -114,6 +118,17 @@ int main(){
                 minasAcertadas++;
             }
         }
+    }
+
+    if(minasAcertadas != minas){
+        printf("Perdiste!!!");
+        printf("\nAcertaste a %i minas.", minasAcertadas);
+    }
+    if(minasAcertadas == minas){
+        mostrarMinas = 0;
+        mostrar_tablero(tableroEnJuego, mostrarMinas);
+        printf("Ganaste!!!");
+        printf("\nMarcaste correctamente las %i minas.", minasAcertadas);
     }
     //LimpiaPantalla();
 
